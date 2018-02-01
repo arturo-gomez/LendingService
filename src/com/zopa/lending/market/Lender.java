7e69125faf910845f1bd30e81851a6ae7fd486e4
Arturo Gomez <a.gomez.del.castillo@gmail.com>
2018-02-01 00:16:24 +0000

    Formatting Lender class

--- src/com/zopa/lending/market/Lender.java
+++ src/com/zopa/lending/market/Lender.java
@@ -2,43 +2,43 @@ package com.zopa.lending.market;
 
 /** Lender abstraction. */
 public class Lender {
-	
-	private final String name;
-	private final Double rate;
-	private final int amount;
-	
-	public Lender(String name, double rate, int amount) {
-		this.name = name;
-		this.rate = rate;
-		this.amount = amount;
-	}
 
-	/** Returns the lender name. */
-	public String getName() {
-		return name;
-	}
+  private final String name;
+  private final Double rate;
+  private final int amount;
 
-	/** Returns the interest rate from this lender. */
-	public Double getRate() {
-		return rate;
-	}
+  public Lender(String name, double rate, int amount) {
+    this.name = name;
+    this.rate = rate;
+    this.amount = amount;
+  }
 
-	/** Returns the amount available from this lender. */
-	public int getAmount() {
-		return amount;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-	  if (obj.getClass() != Lender.class) {
-	    return false;
-	  }
-	  
-	  Lender lender = (Lender) obj;
-	  
-	  return name.equals(lender.getName())
-	      && rate.equals(lender.getRate())
-	      && (amount == lender.getAmount());
-	}
+  /** Returns the lender name. */
+  public String getName() {
+    return name;
+  }
+
+  /** Returns the interest rate from this lender. */
+  public Double getRate() {
+    return rate;
+  }
+
+  /** Returns the amount available from this lender. */
+  public int getAmount() {
+    return amount;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj.getClass() != Lender.class) {
+      return false;
+    }
+
+    Lender lender = (Lender) obj;
+
+    return name.equals(lender.getName()) 
+        && rate.equals(lender.getRate())
+        && (amount == lender.getAmount());
+  }
 
 }
